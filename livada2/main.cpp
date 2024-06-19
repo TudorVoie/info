aae2a913e7413885ace77f84ae287e77f762a9c1
Tudor Voie <tudor_voie@yahoo.com>
2024-06-19 15:25:43 +0300

    Create main.cpp

---
+++ livada2/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+ifstream fin("livada2.in");
+ofstream fout("livada2.out");
+int main()
+{
+    int n, q, v[100001], v1[100001], u = 0, v2[100001] = {0}, st[100001], dr[100001];
+    fin >> n;
+    for(int i = 1; i <= n; i++) fin >> v[i];
+    for(int i = 1; i <= n; i++) {
+        fin >> v1[i];
+        if(v1[i] == 1) u = i;
+        st[i] = u;
+    }
+    u = 0;
+    for(int i = n; i >= 1; i--) {
+        if(v1[i] == 1) u = i;
+        dr[i] = u;
+    }
+    fin >> q;
+    while (q--){
+        int s, x, y, v;
+        fin >> s >> x >> y >> v;
+        if(s == 1){
+            if(dr[y] == 1) y = dr[y];
+        } else {
+            if(st[x] == 1) x = st[x];
+            v = v * (-1);
+        }
+        v2[x] += v;
+        v2[y+1] -= v;
+    }
+    //v[1] = v2[1];
+    //for(int i = 2; i <= n; i++) v[i] = v[i-1] + v2[i];
+    //for(int i = 1; i <= n; i++) fout << v[i] << " ";
+    for(int i = 1; i <= n; i++) {
+        v2[i] += v2[i - 1];
+        v[i] += v2[i];
+    }
+    for(int i = 1; i <= n; i++) cout << v[i] << " ";
+    return 0;
+}
