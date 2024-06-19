8c19aa761bc6ed4d61881ac951357b110a5844f3
Tudor Voie <tudor_voie@yahoo.com>
2024-06-19 15:28:59 +0300

    Update main.cpp

--- livada2/main.cpp
+++ livada2/main.cpp
@@ -34,10 +34,11 @@ int main()
     //v[1] = v2[1];
     //for(int i = 2; i <= n; i++) v[i] = v[i-1] + v2[i];
     //for(int i = 1; i <= n; i++) fout << v[i] << " ";
+    //for(int i = 1; i <= n; i++) v[i] += v2[i] + v2[i-1];
     for(int i = 1; i <= n; i++) {
         v2[i] += v2[i - 1];
         v[i] += v2[i];
     }
-    for(int i = 1; i <= n; i++) cout << v[i] << " ";
+    for(int i = 1; i <= n; i++) fout << v[i] << " ";
     return 0;
 }
