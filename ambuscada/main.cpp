311ae56fa88224c5dc9c9a4c202337be0c6a3acf
Tudor Voie <tudor_voie@yahoo.com>
2024-06-20 17:09:58 +0300

    25 puncte

--- ambuscada/main.cpp
+++ ambuscada/main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 ifstream fin("ambuscada.in");
 ofstream fout("ambuscada.out");
-int a[10];
+vector <int> a;
 int main()
 {
     int n, m, v;
     fin >> n >> m >> v;
-    for(int i = 1; i <= n; i++) a[i] = v;
+    for(int i = 1; i <= n + 1; i++) a.push_back(v);
     //for(int i = 1; i <= n; i++) cout << a[i] << " ";
     while(m--){
         int x, y, d;
         fin >> x >> y >> d;
-        d = d * (-1);
-        a[x]+=v;
-        a[1+y]-=v;
+        //d = d * (-1);
+        a[x]-=d;
+        a[1+y]+=d;
     }
     int cnt = 0;
     for(int i = 1; i <= n; i++){
+        cout << a[i] << " ";
         if(a[i] > 0) cnt++;
     }
     //cout << cnt;
